84e13c731e5b2f5a713141364766d99147a13a40
claudiabraida <braidacreaciones@gamail.com>
2025-02-28 13:32:46 -0300

    modified:   features.js         modified:   index.html         modified:   main.js

--- features.js
+++ features.js
@@ -1,3 +1,5 @@
+const arrayNewCategories = []
+
 function readLocalStorage (key) {
   const datos = JSON.parse(localStorage.getItem(key))
   return datos;
@@ -7,7 +9,17 @@ function saveLocalStorage (key, data) {
   localStorage.setItem(key, JSON.stringify(data))
 }
 
+function arrayCategories (array) {
+  arrayNewCategories.push(array)
+  saveLocalStorage("categoria", arrayNewCategories)
+  readLocalStorage(arrayNewCategories)
+}
+// console.log(arrayNewCategories)
+
+
 export default {
   readLocalStorage,
-  saveLocalStorage
+  saveLocalStorage,
+  arrayCategories
+  
 }
--- main.js
+++ main.js
@@ -1,28 +1,8 @@
+import features from "./features.js"
+
+/* Functions: HTML element selectors */
 const $ = element => document.querySelector(element);
-/* Funciones generales */
-// const showElement = (selectors) => {
-//   for (const selector of selectors) {
-//     selector.classList.toggle("hidden");
-//   }
-// };
-
-// const hideElement = (selectors) => {
-//   for (const selector of selectors) {
-//     selector.classList.add("hidden");
-//   }
-// };
-
-
-// $("#button-hamburger-menu").addEventListener("click", () => {
-//   hideElement([$("#button-hamburger-menu")])
-//   showElement([$("nav"), $("#button-close-menu")])
-// })
-
-// $("#button-close-menu").addEventListener("click", () => {
-//   hideElement([$("#button-close-menu"),$("nav")])
-//   showElement([$("#button-hamburger-menu")])
-// })
- 
+const $$ = element => document.querySelectorAll(element)
 
 const showOrHideElement = (selectors) => {
   for (const selector of selectors) {
@@ -30,7 +10,21 @@ const showOrHideElement = (selectors) => {
   }
 };
 
+const hideElement = (selectors) => {
+  for (const selector of selectors) {
+    selector.classList.add("hidden");
+  }
+};
 
+const showElement = (selectors) => {
+  for (const selector of selectors) {
+    selector.classList.remove("hidden");
+  }
+};
+
+/* _________________ FUNCTIONALITIES OF BUTTONS WITH VIEWS _________________ */
+
+/* ........... buttons header menu ........... */
 $("#button-hamburger-menu").addEventListener("click", () => {
   showOrHideElement([$("#button-hamburger-menu")])
   showOrHideElement([$("nav"), $("#button-close-menu")])
@@ -42,4 +36,70 @@ $("#button-close-menu").addEventListener("click", () => {
   showOrHideElement([$("#button-hamburger-menu")])
 })
 
+/* ........... buttons header nav ........... */
+$("#button-view-balance").addEventListener("click", () => {
+  showElement([$("#view-balance-home")])
+  hideElement([$("#view-categories"), $("#view-report"),$("#new-operation") ])
+})
+
+$("#button-view-categories").addEventListener("click", () => {
+  showElement([$("#view-categories")])
+  hideElement([$("#view-balance-home"), $("#view-report"), $("#new-operation")])
+})
+
+$("#button-view-report").addEventListener("click", () => {
+  showElement([$("#view-report")])
+  hideElement([$("#view-balance-home"), $("#view-categories"), $("#new-operation")])
+})
+
+/* ...... button functionality panel-filters ...... */
+$("#button-hide-panel-filters").addEventListener("click", () => {
+  hideElement([$("#wrapper-panel-filters") ,$("#button-hide-panel-filters")])
+  showElement([$("#button-show-panel-filters")])
+})
+
+$("#button-show-panel-filters").addEventListener("click", () => {
+  showElement([$("#wrapper-panel-filters") ,$("#button-hide-panel-filters")])
+  hideElement([$("#button-show-panel-filters")])
+})
+
+/* ...... button functionality new operation ...... */
+$("#button-new-operation").addEventListener("click", () => {
+  showElement([$("#new-operation")])
+  hideElement([$("#view-balance-home"), $("#view-report")])
+})
+
+
+/* ...... button functionality view categories ...... */
+const $inputnameCategories = $("#name-categories")
+const $buttonAddCategories = $("#add-categories")
+const $filterCategories = $("#filter-categories")
+
+const $containerNameCategories = $("#container-name-categories")
+
+
+function clear (element) {
+  element.value = ""
+}
+
+function addCategory () {
+  const newCategory = $inputnameCategories.value.trim().replace(/\s+/g, " ")
+  $filterCategories.innerHTML += `<option>${newCategory}</option>`
+  
+  clear($inputnameCategories)
+  $containerNameCategories.innerHTML += ` 
+  <div class="flex justify-around p-2">
+    <div class="min-w-12 ">
+     <p class= "bg-yellow-100">${newCategory}</p>
+    </div>
+    <button class="text-blue-700">Editar</button>
+    <button class="text-red-700">Eliminar</button>
+  </div>`
+  
+  features.arrayCategories(newCategory)
+  features.arrayCategories(newCategory)
+}
+
+/* ______________ EVENT ADD CATEGORY ______________ */
+$buttonAddCategories.addEventListener("click", addCategory)
 
